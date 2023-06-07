42ca1f2f6dcd71a8ebc816152642cb7cf5c7a576
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-06-07 12:30:46 -0600

    Tailwind Starters

---
+++ WebpackStarters/Webpack-Tailwind-Starter/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// module.exports = {
+//   content: ["./dist/*.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
